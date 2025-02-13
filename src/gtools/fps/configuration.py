f3c39649ed8d34bc7f072bcb149405cdd4db4666
José Sánchez-Gallego <gallegoj@uw.edu>
2025-02-13 22:55:52 +0000

    Use disabled_robots in read_all_configurations

--- src/gtools/fps/configuration.py
+++ src/gtools/fps/configuration.py
@@ -19,6 +19,24 @@ from rich.progress import track
 from gtools import log
 
 
+REQUIRED_COLS = [
+    "observatory",
+    "MJD",
+    "configuration_id",
+    "design_id",
+    "positionerId",
+    "fiberType",
+]
+BOOLEAN_COLS = ["assigned", "on_target", "disabled", "valid", "decollided", "too"]
+DISABLED_REQUIRED_COLS = REQUIRED_COLS + [
+    "alpha",
+    "beta",
+    "valid",
+    "on_target",
+    "assigned",
+]
+
+
 def read_all_configurations(
     observatories: Sequence[str] = ["APO", "LCO"],
     sdsscore: str | pathlib.Path | None = None,
@@ -58,21 +76,23 @@ def read_all_configurations(
 
     obs_data: list[polars.DataFrame] = []
 
-    required_cols = ["configuration_id", "positionerId", "observatory"]
     if columns is not None:
-        columns = list(set(required_cols).union(set(columns)))
+        columns = list(set(REQUIRED_COLS).union(set(columns)))
 
     for obs in observatories:
         summary_path = sdsscore_path / obs.lower() / "summary_files/"
 
-        files = sorted(summary_path.glob(f"**/confSummary{flavour}*.parquet"))[::-1]
+        files = sorted(summary_path.glob(f"**/confSummary{flavour}*.parquet"))
         summaries: list[polars.DataFrame] = []
         for file in track(files, description=obs, transient=True, console=console):
             try:
-                summary = polars.read_parquet(file)
-                if columns is not None:
-                    valid_cols = [col for col in summary.columns if col in columns]
-                    summary = summary.select(*valid_cols)
+                try:
+                    summary = polars.read_parquet(file, columns=columns)
+                except Exception:
+                    summary = polars.read_parquet(file)
+                    if columns is not None:
+                        valid_cols = [col for col in summary.columns if col in columns]
+                        summary = summary.select(*valid_cols)
 
                 # Some early APO configurations have invalid design_id. Those can
                 # be safely ignored.
@@ -89,13 +109,41 @@ def read_all_configurations(
 
         obs_data.append(polars.concat(summaries, how="diagonal_relaxed"))
 
-    data = polars.concat(obs_data)
+    data = polars.concat(obs_data, how="diagonal_relaxed")
+    del obs_data
+
     data = data.select(
-        *required_cols,
-        *[col for col in data.columns if col not in required_cols],
+        *REQUIRED_COLS,
+        *[col for col in data.columns if col not in REQUIRED_COLS],
     )
 
-    return data.sort(["observatory", "configuration_id", "positionerId"])
+    # Cast boolean columns from integer.
+    data = data.with_columns(
+        polars.col(set(BOOLEAN_COLS).intersection(data.columns)).cast(polars.Boolean)
+    )
+
+    # Add disabled column.
+    if all(col in data.columns for col in DISABLED_REQUIRED_COLS):
+        disabled = (
+            disabled_robots(data)
+            .select(
+                "observatory",
+                "MJD",
+                "positionerId",
+            )
+            .with_columns(disabled=True)
+        )
+        data = (
+            data.drop("disabled", strict=False)
+            .join(
+                disabled,
+                on=["observatory", "MJD", "positionerId"],
+                how="left",
+            )
+            .with_columns(polars.col.disabled.fill_null(False))
+        )
+
+    return data.sort(REQUIRED_COLS)
 
 
 @overload
@@ -133,20 +181,33 @@ def disabled_robots(
     if not isinstance(data, polars.DataFrame):
         data = polars.read_parquet(data)
 
+    # Check that the required columns are present.
+    if not all(col in data.columns for col in DISABLED_REQUIRED_COLS):
+        raise ValueError(
+            "The input data frame is missing required columns. "
+            f"Required columns are: {', '.join(DISABLED_REQUIRED_COLS)}."
+        )
+
     # Cast to proper boolean type.
-    d1 = data.cast(
-        {
-            "decollided": polars.Boolean,
-            "valid": polars.Boolean,
-            "too": polars.Boolean,
-            "assigned": polars.Boolean,
-            "on_target": polars.Boolean,
-        }
-    )
+    d1 = data.cast({"assigned": polars.Boolean, "on_target": polars.Boolean})
 
     pcol = polars.col
     over_c = ["MJD", "positionerId", "observatory"]
     hypot = (pcol.alpha.std().pow(2) + pcol.beta.std().pow(2)).sqrt()
+
+    # Check if the input data frame has a disabled column. In that case get the values
+    # for which the column is not null (that column was only added on 60717). For
+    # those we just use the information in the column.
+    disabled_from_col: polars.DataFrame | None = None
+    if "disabled" in d1.columns:
+        disabled_from_col = d1.filter(polars.col.disabled.is_not_null())
+        disabled_from_col = disabled_from_col.with_columns(
+            disabled=polars.col.disabled.all().over(over_c),
+            alpha_median=pcol.alpha.median().over(over_c),
+            beta_median=pcol.beta.median().over(over_c),
+        )
+
+        d1 = d1.filter(polars.col.disabled.is_null())
 
     # Determine whether a robot was disabled for a given MJD and observatory.
     # We use two metrics: either the robot was never on target for that MJD (even if
@@ -172,6 +233,25 @@ def disabled_robots(
         )
         .with_columns(disabled=(pcol.none_on_target | (pcol.positioner_std < 5)))
     )
+
+    cols = [
+        "MJD",
+        "positionerId",
+        "observatory",
+        "valid",
+        "on_target",
+        "assigned",
+        "alpha",
+        "beta",
+        "disabled",
+        "alpha_median",
+        "beta_median",
+    ]
+    d2 = d2.select(*cols)
+
+    # Join back with the data frame that has the disabled column.
+    if disabled_from_col is not None and disabled_from_col.height > 0:
+        d2 = polars.concat([d2, disabled_from_col.select(*cols)], how="vertical")
 
     # Create a list of disabled robots per MJD and observatory.
     d3 = d2.filter(pcol.disabled).select("observatory", "MJD", "positionerId").unique()
